3b472d9118d3caa4ff9ca91164e1b248c80d123d
Liza-Aprosina <123745170+Liza-Aprosina@users.noreply.github.com>
2024-01-14 12:48:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-
+let links=document.querySelectorAll('.scroll');
+let targeID;
+links.forEach(function (element){
+    element.addEventListener('click', function (event) {
+        event.preventDefault();
+        targetID=element.getAttribute('href');
+        document.querySelector(targetID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    })
+})
